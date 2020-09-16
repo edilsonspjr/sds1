7806040de8cdbd979f33c2437f07b7cfd8452519
Edilson Pontes <edilson.pontes.dev@gmail.com>
2020-09-16 16:28:00 -0300

    Adicionando front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>DSD1 testando som</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
